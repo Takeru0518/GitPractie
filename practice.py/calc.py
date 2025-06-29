cb04d25c1fb5367327fa97c8412c5e13ba5dc44d
takeru.nakanishi <oaoaoaoaoa0518@gmail.com>
2025-06-29 17:12:02 +0900

    python学習　一日目　初回のコミット

---
+++ practice.py/calc.py
@@ -0,0 +1,35 @@
+i = 9 + 2
+print(i)
+print("The result of 9 + 2 is:", i)
+
+i -= 3
+print(i)
+print("3を引いたら:",i)
+
+i *= 3
+print(i)
+print("3を掛けたら:", i)
+
+i /= 5
+print("5で割ったら:", i)    
+
+i //= 2
+print("2で割ったら:", i)
+
+i **= 3
+print("3乗したら:", i)
+
+n = 1
+m = 1.0
+print ("nの型:", type(n))
+print ("mの型:", type(m))
+print ("nの値:", n) 
+print ("mの値:", m)
+
+l = 6.02e23
+print ("lの型:", type(l))
+print ("lの値:", l)
+print("lの値を整数に変換:", int(l))
+print("lの値を文字列に変換:", str(l))
+print("lの値を浮動小数点数に変換:", float(l))
+print("lの値を複素数に変換:", complex(l))
